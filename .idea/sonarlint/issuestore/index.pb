bccd4927e8c6775ccb73b54117558e709c53ab49
smudhiga <smudhiganti@phoenixcontact.com>
2022-02-20 19:10:05 -0500

    Done scraping first web

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+Y
+)first_scrapper/first_scrapper/__init__.py,8\5\85655ef870b6b6ac35390ad36c22abf2d768483b
+Z
+*first_scrapper/first_scrapper/pipelines.py,e\c\ec9b5af0db690706f368f4bb485efa7236a1ca75
+Y
+)first_scrapper/first_scrapper/settings.py,1\4\144505f059bfbc96802af46d90e2f0872a6e7fdf
+I
+first_scrapper/scrapy.cfg,b\4\b4f915550aaac447c65f4665cc2f849423e1e6bd
+\
+,first_scrapper/first_scrapper/middlewares.py,5\1\51edfe9c17fd6c5df623c3af49a1d854ede2d946
+V
+&first_scrapper/first_scrapper/items.py,e\8\e82470c5b42ecdcbb8ac22a6fbb72d2fdbc1efbc
+a
+1first_scrapper/first_scrapper/spiders/__init__.py,e\7\e73ec5f59bb005b7de81d0b224a53717cffde1e8
+]
+-first_scrapper/first_scrapper/spiders/ietf.py,c\3\c3ad75386a07b85d9cfd0535ee9b72c0ddd09b19
--- first_scrapper/first_scrapper/spiders/ietf.py
+++ first_scrapper/first_scrapper/spiders/ietf.py
@@ -4,7 +4,22 @@ import scrapy
 class IetfSpider(scrapy.Spider):
     name = 'ietf'
     allowed_domains = ['pythonscraping.com']
-    start_urls = ['http://pythonscraping.com/']
+    start_urls = ['https://pythonscraping.com/linkedin/ietf.html']
 
     def parse(self, response):
-        pass
+        title = response.css('span.title::text').get()
+        dateFromMeta = response.xpath('/html/head/meta[@name="DC.Date.Issued"]/@content').get()
+        authorName = response.xpath('//div/pre/span[@class="author-name"]/text()').get()
+        authorAddress = response.xpath('//div/pre/span[@class="address"]/text()').get()
+        authorPhone = response.xpath('//div/pre/span[@class="phone"]/text()').get()
+        authorEmail = response.xpath('//div/pre/span[@class="email"]/text()').get()
+        headings = response.xpath('//div/pre/div/span[@class="subheading"]/text()').getall()
+        return {
+                "title": title,
+                "name": authorName,
+                "address": authorAddress,
+                "phone": authorPhone,
+                "email": authorEmail,
+                "headings": headings,
+                "date": dateFromMeta
+                }
