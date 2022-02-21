5d163176a9479af8d75568f3f652ad1de13d8116
smudhiga <smudhiganti@phoenixcontact.com>
2022-02-20 20:22:26 -0500

    wikipideia crwaler working

--- article_scrapper/article_scrapper/spiders/wikipedia.py
+++ article_scrapper/article_scrapper/spiders/wikipedia.py
@@ -1,10 +1,21 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+#from scrapy.contrib.spiders import CrawlSpider, Rule
+#from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
-
-class WikipediaSpider(scrapy.Spider):
+class WikipediaSpider(CrawlSpider):
     name = 'wikipedia'
-    allowed_domains = ['en.wikipidia.org']
-    start_urls = ['http://en.wikipidia.org/']
+    allowed_domains = ['en.wikipedia.org']
+    start_urls = ['https://en.wikipedia.org/wiki/Kevin_Bacon']
 
-    def parse(self, response):
-        pass
+    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
+    #rules = [Rule(LinkExtractor(allow=r'topic/((?!:).)*$'), callback='parse_info', follow=True)]
+    #rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$')), callback='parse_info', follow=True)]
+    #rules = (Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True),)
+    def parse_info(self, response):
+        return {
+            'title': response.xpath('//h1/text()').get(),
+            'url': response.url,
+            'last_edited': response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
+        }
